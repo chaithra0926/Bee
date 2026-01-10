c2370b69b66763411179eecb41967bde44280e6f
Chaithra S Saji <chaithra0926@gmail.com>
2026-01-10 19:20:18 +0530

    Update details logic and styles

--- details.js
+++ details.js
@@ -1,4 +1,18 @@
+function saveDetails() {
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const gender = document.getElementById("gender").value;
 
-function saveDetails(){
- window.location.href="categories.html";
+  firebase.firestore().collection("users").add({
+    name: name,
+    age: age,
+    gender: gender,
+    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+  })
+  .then(() => {
+    window.location.href = "categories.html";
+  })
+  .catch((error) => {
+    alert(error.message);
+  });
 }
